a59452a22cf12a55959082c91cce341fc3f8409e
Thierry Paul <devbox@Thierrys-MacBook-Pro.local>
2021-10-21 13:23:10 -0700

    comments and cleaning

--- order_files_by_date.py
+++ order_files_by_date.py
@@ -7,11 +7,11 @@ which name is the date of last modification to the file.
 """
 
 import os
-import sys
 from datetime import datetime
 import pathlib  # python 3.5+ only
 
-
+# Loop through files in current directory. 
+# If files has .txt extensions, move file
 def move_files():
     directory = os.fsencode(os.getcwd())
     for file in os.listdir(directory):
@@ -20,11 +20,14 @@ def move_files():
             # The date of last modification to file `arg`.
             # timestamp is the number of seconds since the epoch 
             timestamp = os.path.getmtime(filename)
+
+            # Formating the timestamp as YYYY-MM-DD
             formatted_timestamp = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
             
             # create directory for date if it doesn't exist
             pathlib.Path(formatted_timestamp).mkdir(parents=True, exist_ok=True) 
 
+            # Move the file to  `formatted_timestamp/filename` sub directory
             dest = formatted_timestamp + "/" + filename
             os.rename(filename, dest)
 
