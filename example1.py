b4e5d81daf962d65ad3826261c3681a4a6f747ca
Thierry Paul <devbox@Thierrys-MacBook-Pro.local>
2021-10-21 11:09:34 -0700

    wip add loop for all files in dir

--- example1.py
+++ example1.py
@@ -1,24 +1,46 @@
 #!/usr/bin/python3
 
+
+"""
+Place file in a folder which name is the date of last modification of file
+"""
+
 import os
 import sys
 from datetime import datetime
+import pathlib  # python 3.5+ only
 
-# print(str(sys.argv[1]))
-# print(os.getcwd())
 
-# echo myfilename-"`date +"%d-%m-%Y"`
-# os.path.getmtime(path)
 
+# check that at least 1 argument is passed 
 if(len(sys.argv)>1): arg = sys.argv[1]
 else: raise Exception("Must pass 1 argument")
 
 
-# the number of seconds since the epoch 
+# The date of last modification to file `arg`.
+# timestamp is the number of seconds since the epoch 
 timestamp = os.path.getmtime(arg)
 
 formatted_timestamp = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
 
-print(formatted_timestamp)
+# create directory for date if it doesn't exist
+# pathlib.Path(formatted_timestamp).mkdir(parents=True, exist_ok=True) 
 
+# dest = formatted_timestamp + "/" + arg
+# os.rename(arg, dest)
+
+def move_files():
+    directory = os.fsencode(os.getcwd())
+    
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".txt"): 
+            # print(os.path.join(directory, filename))
+            print(filename)
+            continue
+        else:
+            continue
+
+
+move_files()
 
