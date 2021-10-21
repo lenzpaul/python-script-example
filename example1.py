5c127919a46ef7b5484f35815df5df60bdd327ba
Thierry Paul <devbox@Thierrys-MacBook-Pro.local>
2021-10-21 11:14:29 -0700

    applied fx to all files in dir

--- example1.py
+++ example1.py
@@ -2,7 +2,8 @@
 
 
 """
-Place file in a folder which name is the date of last modification of file
+For each file with .txt extension in this directory, move the file to subdirectory 
+which name is the date of last modification to the file.
 """
 
 import os
@@ -11,32 +12,22 @@ from datetime import datetime
 import pathlib  # python 3.5+ only
 
 
-
-# check that at least 1 argument is passed 
-if(len(sys.argv)>1): arg = sys.argv[1]
-else: raise Exception("Must pass 1 argument")
-
-
-# The date of last modification to file `arg`.
-# timestamp is the number of seconds since the epoch 
-timestamp = os.path.getmtime(arg)
-
-formatted_timestamp = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
-
-# create directory for date if it doesn't exist
-# pathlib.Path(formatted_timestamp).mkdir(parents=True, exist_ok=True) 
-
-# dest = formatted_timestamp + "/" + arg
-# os.rename(arg, dest)
-
 def move_files():
     directory = os.fsencode(os.getcwd())
-    
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".txt"): 
-            # print(os.path.join(directory, filename))
-            print(filename)
+            # The date of last modification to file `arg`.
+            # timestamp is the number of seconds since the epoch 
+            timestamp = os.path.getmtime(filename)
+            formatted_timestamp = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+            
+            # create directory for date if it doesn't exist
+            pathlib.Path(formatted_timestamp).mkdir(parents=True, exist_ok=True) 
+
+            dest = formatted_timestamp + "/" + filename
+            os.rename(filename, dest)
+
             continue
         else:
             continue
